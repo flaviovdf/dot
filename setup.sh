3729eee1d099d5a315a67d9d88c620d8fd0e1223
Flavio Figueiredo <flaviovdf@gmail.com>
2017-10-14 10:15:50 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@ ln -s $HOME/dot/bashrc $HOME/.bashrc
 
 ln -s $HOME/dot/vim $HOME/.vim
 ln -s $HOME/dot/vimrc $HOME/.vimrc
-if [ ! -e $HOME/.config/ ]; then
+if [[ ! -e $HOME/.config/ ]]; then
     mkdir $HOME/.config/
 fi
 ln -s $HOME/dot/vim ~/.config/nvim
-git submodule update --init --recursive
+
 echo "Install python3 and neovim"
 echo "Install ctags"
 echo "Execute pip3 install neovim"
