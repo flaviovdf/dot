186da4a5d253fd832f6972141089003cc9da0012
Flavio Figueiredo <flaviovdf@gmail.com>
2017-10-14 10:13:31 -0300

    Update exports.sh

--- exports.sh
+++ exports.sh
@@ -15,12 +15,12 @@ export LS_COLORS=$LS_COLORS:'di=0;36:ln=0;34:ex=0;32:mi=0;31'
 
 # set's current working dir for terminals that do not support it
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-T=$(basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat \
-  | cut -d \  -f 4) | tail -1 | sed 's/^.* //'))
-if [[ $T == "yakuake" ]]; then
-  if [[ -e /dev/shm/$USER-yakuake-cwd ]]; then
-    cd "$(cat /dev/shm/$USER-yakuake-cwd)"
+  T=$(basename "/"$(ps -f -p $(cat /proc/$(echo $$)/stat \
+    | cut -d \  -f 4) | tail -1 | sed 's/^.* //'))
+  if [[ $T == "yakuake" ]]; then
+    if [[ -e /dev/shm/$USER-yakuake-cwd ]]; then
+      cd "$(cat /dev/shm/$USER-yakuake-cwd)"
+    fi
+    alias cd='function __cd() {cd $* && pwd > /dev/shm/$USER-yakuake-cwd}; __cd'
   fi
-  alias cd='function __cd() {cd $* && pwd > /dev/shm/$USER-yakuake-cwd}; __cd'
-fi
 fi
